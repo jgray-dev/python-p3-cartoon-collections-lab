d2b3de65833f9d91c8a847622d3afc8e47a1e11a
Jackson Gray <nohaxjustdoge@gmail.com>
2024-04-01 20:04:54 +0000

    pray i never meet the person responsible for python... many many words will be exchanged (and not nice ones eithergit add .)

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarfs):
+    i = 1
+    for dwarf in dwarfs:
+        print(f"{i}. {dwarf}")
+        i += 1
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f"{each.title()}!" for each in planeteer_calls]
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(planeteer_calls):
+    for call in planeteer_calls:
+        if len(call) > 4:
+            return True
+    return False
+
+
+cheeses = ["camembert", "gouda", "cheddar"]
+
+
+def find_the_cheese(list):
+    for ing in list:
+        if ing in cheeses:
+            return ing
+    return None
